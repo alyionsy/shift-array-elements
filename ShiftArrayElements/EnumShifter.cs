feac26ec12a613515abaec4c0afb7e33b9f8fde1
Albina Bondarchik <erionolsen@gmail.com>
2021-05-01 23:43:46 +0300

    Add ShiftArrayElements project

---
+++ ShiftArrayElements/EnumShifter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ShiftArrayElements
+{
+    public static class EnumShifter
+    {
+        /// <summary>
+        /// Shifts elements in a <see cref="source"/> array using directions from <see cref="directions"/> array, one element shift per each direction array element.
+        /// </summary>
+        /// <param name="source">A source array.</param>
+        /// <param name="directions">An array with directions.</param>
+        /// <returns>An array with shifted elements.</returns>
+        /// <exception cref="ArgumentNullException">source array is null.</exception>
+        /// <exception cref="ArgumentNullException">directions array is null.</exception>
+        /// <exception cref="InvalidOperationException">direction array contains an element that is not <see cref="Direction.Left"/> or <see cref="Direction.Right"/>.</exception>
+        public static int[] Shift(int[] source, Direction[] directions)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            
+            if (directions is null)
+            {
+                throw new ArgumentNullException(nameof(directions));
+            }
+
+            for (int i = 0; i < directions.Length; i++)
+            {
+                Direction currentDirection = directions[i];
+                switch (currentDirection)
+                {
+                    case Direction.Left:
+                    {
+                        for (int j = 0; j < source.Length - 1; j++)
+                        {
+                            source[j] += source[j + 1];
+                            source[j + 1] = source[j] - source[j + 1];
+                            source[j] -= source[j + 1];
+                        }
+                        
+                        break;
+                    }
+
+                    case Direction.Right:
+                    {
+                        for (int j = source.Length - 1; j > 0; j--)
+                        {
+                            source[j] += source[j - 1];
+                            source[j - 1] = source[j] - source[j - 1];
+                            source[j] -= source[j - 1];
+                        }
+                        
+                        break;
+                    }
+
+                    default:
+                        throw new InvalidOperationException($"Incorrect {currentDirection} enum value.");
+                }
+            }
+
+            return source;
+        }
+    }
+}
---
+++ ShiftArrayElements/Shifter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ShiftArrayElements
+{
+    public static class Shifter
+    {
+        /// <summary>
+        /// Shifts elements in a <see cref="source"/> array using <see cref="iterations"/> array for getting directions and iterations (odd elements - left direction, even elements - right direction).
+        /// </summary>
+        /// <param name="source">A source array.</param>
+        /// <param name="iterations">An array with iterations.</param>
+        /// <returns>An array with shifted elements.</returns>
+        /// <exception cref="ArgumentNullException">source array is null.</exception>
+        /// <exception cref="ArgumentNullException">iterations array is null.</exception>
+        public static int[] Shift(int[] source, int[] iterations)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            
+            if (iterations is null)
+            {
+                throw new ArgumentNullException(nameof(iterations));
+            }
+
+            for (int i = 0; i < iterations.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (int j = 0; j < iterations[i]; j++)
+                    {
+                        int lastElement = source[0];
+                        Array.Copy(source, 1, source, 0, source.Length - 1);
+                        source[^1] = lastElement;
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < iterations[i]; j++)
+                    {
+                        int firstElement = source[^1];
+                        Array.Copy(source, 0, source, 1, source.Length - 1);
+                        source[0] = firstElement;
+                    }
+                }
+                
+                Console.WriteLine(string.Join(" ", source));
+            }
+
+            return source;
+        }
+    }
+}
